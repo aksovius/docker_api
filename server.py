1cee4627753592ca3c3a9a7cfcc483071e717816
Alexander Kim <alexe91@gmail.com>
2023-04-25 11:54:23 +0900

    add image upload endpoint

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 import strawberry
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from strawberry.asgi import GraphQL
 import mutations
 import queries
+from utils import *
+from typing import List, Optional
+
+DATA_DIR = '/home/gil/Desktop/class_files'
 
 @strawberry.type
 class Query:
@@ -22,6 +26,38 @@ graphql_app = GraphQL(schema)
 app = FastAPI()
 app.add_route("/graphql", graphql_app)
 
+
+@app.get('/file')
+async def result_endpoint(folder: str, filename: str):
+  
+    path = os.path.join(DATA_DIR, folder, filename)
+    
+    # check if result folder exist
+    if os.path.isfile(path):
+        return FileResponse(path=path, filename=filename) 
+    # if file not found, return status
+    else:
+        return {"status": 'Not found'}
+           
+@app.post('/file')
+async def file_upload_endpoint(folder: Optional[str] ="temp", 
+                              file: List[UploadFile] = File(...)):
+    print(folder)
+    
+    upload_dir = os.path.join(DATA_DIR, folder)
+    await upload_file(file, upload_dir)
+    return {"success": 1}
+
+# upload 1 image for pages
+@app.post('/image')
+async def image_upload_endpoint(folder: Optional[str] ="temp", 
+                              image: UploadFile = File(...)):
+    
+    print(folder)
+    print(image)
+    upload_dir = os.path.join(DATA_DIR, folder)
+    await upload_one_file(image, upload_dir)
+    return {"success": 1, "file": {"url": f"https://210.102.178.108.nip.io/resource/file?folder={folder}&filename={image.filename}"}}
 # @app.websocket("/graphql/wss")
 # async def websocket_endpoint(websocket: WebSocket):
 #     sender = websocket.cookies.get("X-Authorization")
---
+++ utils.py
@@ -0,0 +1,88 @@
+import os
+from werkzeug.utils import secure_filename
+import aiofiles
+from fastapi.exceptions import HTTPException
+import zipfile
+from io import BytesIO
+from fastapi.responses import FileResponse, StreamingResponse
+
+ALLOWED_EXTENSIONS = {'doc', 'ppt', 'pdf', 'docs', 'zip', 'pptx, jpg, jpeg, png'}
+CHUNK_SIZE = 1024 * 1024
+
+def allowed_file(filename, ALLOWED_EXTENSIONS):
+    """Check if the file extension is allowed."""
+    # Get the file extension
+    extension = filename.rsplit('.', 1)[1].lower()
+    # Check if the extension is allowed
+    if extension in ALLOWED_EXTENSIONS:
+        return True
+    else:
+        return False
+    
+async def upload_file(file, upload_dir):
+    
+    # Check if directory exists, if not create it
+    isExist = os.path.exists(upload_dir)
+    if not isExist:
+        os.makedirs(upload_dir) 
+    # Loop through uploaded files and save them
+    for f in file:
+        # Check if file has a name
+        if f.filename == '':
+            next()
+        # Check if file has correct file extension
+        if f and allowed_file(f.filename, ALLOWED_EXTENSIONS):
+            filename = secure_filename(f.filename)
+            # Save file
+            try:
+                filepath = os.path.join(upload_dir, filename)
+                async with aiofiles.open(filepath, 'wb') as data:
+                    while chunk := await f.read(CHUNK_SIZE):
+                        await data.write(chunk)
+            except Exception:
+                raise HTTPException(status_code=500, 
+                    detail='There was an error uploading the file')
+            finally:
+                await f.close()
+                print(filename, " saved")
+
+async def upload_one_file(file, upload_dir):
+    
+    # Check if directory exists, if not create it
+    isExist = os.path.exists(upload_dir)
+    if not isExist:
+        os.makedirs(upload_dir) 
+    # Loop through uploaded files and save them
+   
+    # Check if file has a name
+    if file.filename == '':
+        next()
+    # Check if file has correct file extension
+    if file and allowed_file(file.filename, "jpg, jpeg, png"):
+        filename = secure_filename(file.filename)
+        # Save file
+        try:
+            filepath = os.path.join(upload_dir, filename)
+            async with aiofiles.open(filepath, 'wb') as data:
+                while chunk := await file.read(CHUNK_SIZE):
+                    await data.write(chunk)
+        except Exception:
+            raise HTTPException(status_code=500, 
+                detail='There was an error uploading the file')
+        finally:
+            await file.close()
+            print(filename, " saved")
+
+def zipfiles(file_list, study):
+    io = BytesIO()
+    zip_filename = "%s.zip" % study
+    with zipfile.ZipFile(io, mode='w', compression=zipfile.ZIP_DEFLATED) as zip:
+        for fpath in file_list:
+            zip.write(fpath, fpath.rsplit('/', 1)[1])
+        #close zip
+        zip.close()
+    return StreamingResponse(
+        iter([io.getvalue()]),
+        media_type="application/x-zip-compressed",
+        headers = { "Content-Disposition":f"attachment;filename=%s" % zip_filename}
+    )
